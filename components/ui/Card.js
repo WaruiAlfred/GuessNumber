07d5ee154c48c80b180ef1457dc5b1e92d8b53a2
WaruiAlfred <alfredwarui17@gmail.com>
2022-12-26 16:45:42 +0300

    create custom card component

---
+++ components/ui/Card.js
@@ -0,0 +1,26 @@
+import { StyleSheet, View } from "react-native";
+import Colors from "../../constants/colors";
+
+function Card({ children }) {
+  return <View style={styles.card}>{children}</View>;
+}
+
+export default Card;
+
+const styles = StyleSheet.create({
+  card: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 36,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    padding: 16,
+    backgroundColor: Colors.primary800,
+    elevation: 4, // android
+    //iOS
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
